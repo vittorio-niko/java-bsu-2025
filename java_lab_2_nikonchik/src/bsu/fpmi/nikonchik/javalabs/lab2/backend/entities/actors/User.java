721cf2335eb9247b8526fdead72156d57c01dbb9
vittorio-niko <viktor.nikonchik2007@gmail.com>
2025-11-14 18:37:31 +0300

    Created bank product class

---
+++ java_lab_2_nikonchik/src/bsu/fpmi/nikonchik/javalabs/lab2/backend/entities/actors/User.java
@@ -0,0 +1,4 @@
+package bsu.fpmi.nikonchik.javalabs.lab2.backend.entities.actors;
+
+public class User {
+}
---
+++ java_lab_2_nikonchik/src/bsu/fpmi/nikonchik/javalabs/lab2/backend/entities/products/Product.java
@@ -0,0 +1,52 @@
+package bsu.fpmi.nikonchik.javalabs.lab2.backend.entities.products;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public abstract class Product implements Serializable,
+        Comparable<Product>, Cloneable {
+    private static final long serialVersionUID = 1L;
+
+    protected final UUID productId;
+    protected UUID ownerId;
+    protected final LocalDateTime createdDate;
+
+    protected Product(UUID productId, UUID ownerId, LocalDateTime createdDate) {
+        this.productId = Objects.requireNonNull(productId,
+                "Product id cannot be null");
+        this.ownerId = Objects.requireNonNull(ownerId,
+                "Owner id cannot be null");
+        this.createdDate = Objects.requireNonNull(createdDate,
+                "Creation date cannot be null");
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return this.createdDate.compareTo(other.createdDate);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Product cloned = (Product) super.clone();
+        return cloned;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product that = (Product) o;
+        return Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+
+    public UUID getProductId() { return productId; }
+    public LocalDateTime getCreatedDate() { return createdDate; }
+    public UUID getOwnerId() { return ownerId; }
+}
